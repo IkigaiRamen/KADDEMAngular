d103540ea583700a23dcf8fd66eedea01a5071b6
IkigaiRamen <55280193+IkigaiRamen@users.noreply.github.com>
2022-12-13 12:37:55 +0100

    Initial commit

---
+++ src/app/core/services/contrat.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Contrat } from '../Model/Contrat';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class ContratService {
+  public uri = 'http://localhost:9090/kaddem/Contrat';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+  constructor(private http: HttpClient) {}
+  getAllContrat() {
+    return this.http.get<Contrat[]>(this.uri + '/getAllContrat');
+  }
+  addContrat(c: Contrat) {
+    //console.log('c:==================> ', c);
+    return this.http.post(this.uri + '/addContrat', c);
+  }
+
+  updateContrat(contrat: Contrat): Observable<Object> {
+    return this.http.put<Contrat>(this.uri + '/updateContrat', contrat);
+  }
+
+  getContratById(id: number) {
+    return this.http.get<Contrat>(this.uri + `/getbyidContrat/${id}`);
+  }
+
+  deleteContrat(id: number): Observable<Object> {
+    return this.http.delete(this.uri + `/deleteContrat/${id}`);
+  }
+}
---
+++ src/app/core/services/universite.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Universite } from '../Model/Universite';
+@Injectable({
+  providedIn: 'root',
+})
+export class UniversiteService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+  constructor(private http : HttpClient) { }
+  url: string = 'http://localhost:9090/kaddem/Universite';
+
+
+  allUni(): Observable<any> {
+    return this.http.get(this.url + `/getAllUniversite`);
+  }
+  addUniv(universite: Universite){
+    return this.http.post(this.url+`/addUniversite`, universite);
+  }
+ 
+ deleteUni(idUni: number): Observable<Object> {
+    return this.http.delete(this.url + `/deleteUniversite/${idUni}`);
+  }
+  getUniversiteById(idUni: number): Observable<Universite> {
+    return this.http.get<Universite>(this.url + `/getbyid/${idUni}`);
+  } 
+  updateUni(universite: Universite ): Observable<Universite> {
+    return this.http.put<Universite>(this.url + `/updateUniversite/`,universite );
+  }
+  
+  
+}
